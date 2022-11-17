527cdcd3a189f25c2ec5a06d6fc9704465f0c1f0
Prakash Naikwadi <naikwadiprakash1@gmail.com>
2022-11-18 00:01:47 +0530

    Implemented a Basic Desktop & Mobile Navigation and added animation to side drawer using react-transition-group library

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -1,22 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import MainHeader from "./MainHeader";
 import "./MainNavigation.css";
+import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElements/Backdrop";
 
 function MainNavigation(props) {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">Your Places</Link>
-      </h1>
-      <nav>...</nav>
-    </MainHeader>
+    <>
+      {drawerIsOpen && (
+        <Backdrop onClick={() => setDrawerIsOpen((prev) => !prev)} />
+      )}
+
+      <SideDrawer
+        show={drawerIsOpen}
+        onClick={() => setDrawerIsOpen((prev) => !prev)}
+      >
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+
+      <MainHeader>
+        <button
+          className="main-navigation__menu-btn"
+          onClick={() => setDrawerIsOpen((prev) => !prev)}
+        >
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">Your Places</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </>
   );
 }
 
